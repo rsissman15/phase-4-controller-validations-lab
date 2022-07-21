e194bc956f4df9a40e5ce4c259ac13e40a3d93d1
Rebecca Sissman <rsissman15@aol.com>
2022-07-21 15:53:30 -0400

    commit tree 0d772382f2d18c78a6f7fc30c0a9f8494c911551

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,13 +1,19 @@
 class AuthorsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   
   def show
-    author = Author.find(params[:id])
+    author = find_author
 
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
+
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
@@ -17,5 +23,18 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  def find_author
+    Author.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json:{error:"Author not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
   
 end

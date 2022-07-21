f84c3efdefab08b3a01e58c5731a4de507b4052c
Rebecca Sissman <rsissman15@aol.com>
2022-07-21 15:53:46 -0400

    Completed Assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
-    post = Post.find(params[:id])
+    post = find_post
     
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
+    post = find_post
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +22,18 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json:{error:"Post not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
